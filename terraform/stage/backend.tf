07d3a248ea5a3a46f4a78c047c4c725e6ed0c33b
Mikhail Klyuev <c32exe@gmail.com>
2021-01-11 15:33:29 +0300

    add stage tf infra

---
+++ terraform/stage/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "devoptus-tfstate"
+    region     = "us-east-1"
+    key        = "stage.tfstate"
+    access_key = "nsBaotZ7bPkM35cMUz94"
+    secret_key = "k71COGq7MhLUBDLtZyfj1CEjhRdvTAFl_w9JR0vN"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
