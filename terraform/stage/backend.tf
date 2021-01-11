a8859cb009b37834d7ea5cf87f4c49cbbbd6e756
Mike <60885198+Mike-ntrt@users.noreply.github.com>
2021-01-11 15:51:06 +0300

    Update backend.tf

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -5,7 +5,7 @@ terraform {
     region     = "us-east-1"
     key        = "stage.tfstate"
     access_key = "nsBaotZ7bPkM35cMUz94"
-    secret_key = "k71COGq7MhLUBDLtZyfj1CEjhRdvTAFl_w9JR0vN"
+    secret_key = "xxxxxxxxxxxxxxxxxx"
 
     skip_region_validation      = true
     skip_credentials_validation = true
