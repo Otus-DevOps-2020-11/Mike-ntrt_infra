fe0e145cfffdf5f6d13df090a720234e85ae64f5
Mikhail Klyuev <c32exe@gmail.com>
2021-01-18 02:01:19 +0300

    change grep target

--- ansible/dynamic-inventory.json.sh
+++ ansible/dynamic-inventory.json.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yc_instances_app=($(yc compute instance list | grep app | awk -F\| '{print $3 $6}'))
-yc_instances_db=($(yc compute instance list | grep db |  awk -F\| '{print $3 $6}'))
+yc_instances_app=($(yc compute instance list | grep reddit-app | awk -F\| '{print $3 $6}'))
+yc_instances_db=($(yc compute instance list | grep reddit-db |  awk -F\| '{print $3 $6}'))
 
 if [[ "$1" == "--list" ]]; then
 #heredoc
